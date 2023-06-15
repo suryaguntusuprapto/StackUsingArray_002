4a1dc4af3fc7611c01e124e88c80b642f4561db4
suryaguntursuprapto <suryaguntur2000@gmail.com>
2023-06-15 10:34:27 +0700

    mengisi member pada class program

--- StackUsingArray_002/StackUsingArray_002.cpp
+++ StackUsingArray_002/StackUsingArray_002.cpp
@@ -18,5 +18,13 @@ public:
 			cout << "Stack Full" << endl;
 			return "";
 		}
+
+		top++;
+		stack_array[top] = element;
+
+		cout << endl;
+		cout << element << "ditambahkan(pushed)." << endl;
+
+		return element;
 	}
 };
