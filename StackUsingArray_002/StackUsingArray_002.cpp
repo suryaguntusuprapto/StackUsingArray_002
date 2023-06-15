f8658199365e01d371d3b8ce1c0164337609620f
suryaguntursuprapto <suryaguntur2000@gmail.com>
2023-06-15 10:29:06 +0700

    mengisi constructor method buat ngepush

--- StackUsingArray_002/StackUsingArray_002.cpp
+++ StackUsingArray_002/StackUsingArray_002.cpp
@@ -7,4 +7,16 @@ class program {
 private:
 	string stack_array[5];
 	int top;
+
+public:
+	program() {
+		top = -1;
+	}
+
+	string push(string element) {
+		if (top == 4) {
+			cout << "Stack Full" << endl;
+			return "";
+		}
+	}
 };
